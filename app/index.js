200e7639274a5570cea4bdb46903d28a603dd6f7
Rodrigo Villalba <rodris.jvo@gmail.com>
2019-07-31 15:29:45 -0400

    Refactor index.js to hooks

--- app/index.js
+++ app/index.js
@@ -10,37 +10,36 @@ const Popular = React.lazy(() => import('./components/Popular'))
 const Battle = React.lazy(() => import('./components/Battle'))
 const Results = React.lazy(() => import('./components/Results'))
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
 
-              <React.Suspense fallback={<Loading />} >
-                <Switch>
-                  <Route exact path='/' component={Popular} />
-                  <Route exact path='/battle' component={Battle} />
-                  <Route path='/battle/results' component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
+
+function App() {
+  const [theme, setTheme] = React.useState('light');
+  const toggleTheme = () => {
+    setTheme((theme) => theme === 'light' ? 'dark' : 'light');
   }
+  const themeProviderValue = React.useMemo(() => ({theme, toggleTheme}), [theme]);
+ 
+  return (
+    <Router>
+      <ThemeProvider value={themeProviderValue}>
+        <div className={theme}>
+          <div className='container'>
+            <Nav />
+
+            <React.Suspense fallback={<Loading />} >
+              <Switch>
+                <Route exact path='/' component={Popular} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
+      
 }
 
 ReactDOM.render(
