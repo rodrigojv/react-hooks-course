e1bdb926b3a52cd7def752bd044f99f096e22c70
Rodrigo Villalba <rodris.jvo@gmail.com>
2019-08-06 15:25:04 -0400

    Refactor Results to hooks

--- app/components/Results.js
+++ app/components/Results.js
@@ -47,72 +47,89 @@ ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
 }
 
-export default class Results extends React.Component {
-  state = {
+function resultsReducer(state, action) {
+  if (action.type === 'success') {
+    return {
+      ...state,
+      winner: action.winner,
+      loser: action.loser,
+      error: null,
+      loading: false
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      loading: false,
+      error: action.message
+    }
+  } else {
+    throw new Error(`Actoin ${action.type} not recognized`);
+  }
+}
+
+export default function Results({location}) {
+  const { playerOne, playerTwo } = queryString.parse(location.search)
+  const [state, dispatch] = React.useReducer(resultsReducer, {
     winner: null,
     loser: null,
     error: null,
     loading: true
-  }
-  componentDidMount () {
-    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
+  });
 
+  React.useEffect(() => {
     battle([ playerOne, playerTwo ])
       .then((players) => {
-        this.setState({
+        dispatch({ type: "success",
           winner: players[0],
-          loser: players[1],
-          error: null,
-          loading: false
+          loser: players[1]
         })
       }).catch(({ message }) => {
-        this.setState({
-          error: message,
-          loading: false
+        dispatch({ type : "error",
+          message
         })
       })
+  }, [playerOne, playerTwo]);
+
+  const { winner, loser, error, loading } = state;
+  
+  if (loading === true) {
+    return <Loading text='Battling' />
   }
-  render() {
-    const { winner, loser, error, loading } = this.state
 
-    if (loading === true) {
-      return <Loading text='Battling' />
-    }
-
-    if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
-    }
-
+  if (error) {
     return (
-      <React.Fragment>
-        <div className='grid space-around container-sm'>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Winner'}
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile}/>
-          </Card>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Loser'}
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            name={loser.profile.login}
-            href={loser.profile.html_url}
-          >
-            <ProfileList profile={loser.profile}/>
-          </Card>
-        </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
-        </Link>
-      </React.Fragment>
+      <p className='center-text error'>{error}</p>
     )
   }
+
+
+  return (
+    <React.Fragment>
+      <div className='grid space-around container-sm'>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile}/>
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ProfileList profile={loser.profile}/>
+        </Card>
+      </div>
+      <Link
+        to='/battle'
+        className='btn dark-btn btn-space'>
+          Reset
+      </Link>
+    </React.Fragment>
+  )
 }
